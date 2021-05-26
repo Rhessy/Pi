f604ddb89c2b403ad51a912ac034c7b2ff12fbf3
Rhessy <Riley_Hess@aol.com>
2021-05-25 20:05:05 -0900

    Adding website directory to github for version control

---
+++ html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head> 
+<link rel="stylesheet" href="style.css"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<h1>Riley Hess</h1>
+</head>
+<div class="nav">
+  <a href="index.php" class="navbutton">Home</a>
+  <a href="Skills.html" class="navbutton">Skills </a>
+  <a href="Experience.html" class="navbutton">Experience </a>
+  <a href="Links.html" class="navbutton">Links</a>
+  <a href="Resume.html" class="navbutton">Resume</a>
+  <a href="Contact.html" class="navbutton">Contact</a>
+  <a href="https://www.linkedin.com/in/riley-hess-1625a61b4?lipi=urn%3Ali%3Apage%3Ad_
+  flagship3_profile_view_base_contact_details%3B8wszbtKsRdOkYPFFY3HTYw%3D%3D" class="navbutton">LinkedIn Profile</a>
+</div>
+
+
+
+<img src="ProfilePic.JPG" alt="Profile Picture" height="400" width="300">
+
+
+</html>
